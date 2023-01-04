f877711f82e06a55bccdc2b14ddebefc5f421258
Muhammadjon <myaxyo@yahoo.com>
2023-01-04 10:50:36 +0100

    OpenAI backend finished

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
+const cors = require("cors");
 
 const app = express();
 
+app.use(cors());
+
 //enable body parser
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
